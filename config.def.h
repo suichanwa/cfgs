4a37832b5ac0f65b4dfb2aed82f256b3dbb691bf
suichanwa <91247676+suichanwa@users.noreply.github.com>
2022-03-13 19:15:07 +0200

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -5,13 +5,13 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "hack:size=10" };
+static const char *fonts[]          = { "hack:size=11" };
 static const char dmenufont[]       = "hack:size=10";
 static const char col_gray1[]       = "#1d2021";
 static const char col_gray2[]       = "#665c54";
 static const char col_gray3[]       = "#D5C4A1";
 static const char col_gray4[]       = "#fbf1c7";
-static const char col_cyan[]        = "#D5C4A1";
+static const char col_cyan[]        = "#1d2021";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "terminator", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
