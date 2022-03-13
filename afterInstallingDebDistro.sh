2ffe120c2dac1c7ffbf3e508bdd18149ef71c626
suichanwa <91247676+suichanwa@users.noreply.github.com>
2022-03-13 17:31:33 +0200

    Update afterInstallingDebDistro.sh

--- afterInstallingDebDistro.sh
+++ afterInstallingDebDistro.sh
@@ -1,2 +1,9 @@
 sudo apt-get update
 sudo apt-get upgrade
+
+1sudo apt-get install build-essential libx11-dev libxinerama-dev sharutils suckless-tools libxft-dev stterm
+cd /usr/local/src
+sudo wget http://dl.suckless.org/dwm/dwm-6.0.tar.gz
+sudo tar xvzf dwm-6.0.tar.gz
+chown -R `id -u`:`id -g` dwm-6.0
+cd dwm-6.0
